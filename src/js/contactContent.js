0b15dfc1aaaca7ec21eb25ddde468ab23a879fc8
Carlos Del Real <carloshdelreal@gmail.com>
2019-11-07 13:10:44 -0500

    add rodizio background to the contact content

--- src/js/contactContent.js
+++ src/js/contactContent.js
@@ -1,7 +1,10 @@
 import { createDiv } from './elmcreator';
+import logoBack from '../img/logo-rodizio.png';
 
 export default function createContactContent() {
   const contactContent = createDiv(['jumbotron', 'jumbotron-fluid']);
+  contactContent.style.background = `url('${logoBack}') no-repeat`;
+  contactContent.style.backgroundPosition = 'center center';
   const contactTitle = createDiv(['display-4']);
   contactTitle.innerText = 'Contact us';
   const contactText = createDiv(['lead']);
